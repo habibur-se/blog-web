ce91ff5466d1da07a0480de061013cc5a8fbefb3
habibur-se <habibur13rk@gmail.com>
2025-04-05 00:03:19 +0600

    changes

--- resources/views/admin/index.php
+++ resources/views/admin/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Login Site</title>
+    <title>Admin Login Blog Site </title>
 </head>
 <body>
     <h1>Admin Home</h1>
